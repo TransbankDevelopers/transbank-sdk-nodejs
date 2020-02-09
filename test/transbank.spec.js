798e48a80954a2b2d47cb26727e340c1d79e8d5c
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2020-02-09 00:36:43 -0300

    fix(test): second test passing, first transaction sent

--- test/transbank.spec.js
+++ test/transbank.spec.js
@@ -8,5 +8,28 @@ describe('Transbank API', () => {
       const tbk = new Transbank();
       expect(tbk).toBeDefined();
     });
+    it('Debe poder hacer una transacción de prueba usando configuración webpay normal', (done) => {
+      const tbk = new Transbank();
+      tbk.initTransaction({
+        wSTransactionType: 'TR_NORMAL_WS',
+        buyOrder: 'testOrder',
+        sessionId: 1,
+        returnURL: 'http://aTestUrl.com',
+        finalURL: 'http://aFinalTestUrl.com',
+        transactionDetails: {
+          amount: 1000,
+          commerceCode: tbk.configuration.commerceCode,
+          buyOrder: 1,
+        },
+      })
+        .then((response) => {
+          expect(response)
+            .toEqual(expect.objectContaining({
+              token: expect.any(String),
+            }));
+          done();
+        })
+        .catch((tbkError) => expect(tbkError).toBeUndefined())
+    });
   });
 });
