01be0a59ee1b135e039274aae2f45573c9577815
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2020-02-12 22:56:34 -0300

    feat(refactor): match syntax from other TBK APIs

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const Transbank = require('./lib/transbank');
+const Webpay = require('./lib/webpay');
 const Configuration = require('./lib/config');
 const { Environment, services, environments } = require('./lib/environments');
 
-Transbank.Environment = Environment;
-Transbank.Configuration = Configuration;
-Transbank.services = services;
-Transbank.environments = environments;
-module.exports = Transbank;
+Webpay.Environment = Environment;
+Webpay.Configuration = Configuration;
+Webpay.services = services;
+Webpay.environments = environments;
+module.exports = Webpay;
---
+++ test/webpay.spec.js
@@ -0,0 +1,72 @@
+const Webpay = require('../index');
+
+jest.setTimeout(10000);
+
+describe('Transbank API', () => {
+  describe('Creación y configuración de objeto principal', () => {
+    it('Debe crear correctamente el objeto', () => {
+      const tbk = new Webpay();
+      expect(tbk).toBeDefined();
+    });
+    it('Debe poder hacer una transacción de prueba usando configuración webpay normal', (done) => {
+      const tbk = new Webpay()
+        .withConfiguration(Webpay.Configuration.forTestingWebpayPlusNormal())
+        .getNormalTransaction();
+
+      tbk.initTransaction(1000, 1, 1, 'http://aTestUrl.com', 'http://aFinalTestUrl.com')
+        .then((response) => {
+          expect(response)
+            .toEqual(expect.objectContaining({
+              token: expect.any(String),
+            }));
+          done();
+        })
+        .catch((tbkError) => expect(tbkError).toBeUndefined());
+    });
+  });
+});
+
+describe('Test de servicios de Transbank | initTransaction de cada uno de ellos', () => {
+  describe('Transacción Normal', () => {
+    it('Ambiente Integración', (done) => {
+      const tbk = new Webpay()
+        .withConfiguration(Webpay.Configuration.forTestingWebpayPlusNormal())
+        .getNormalTransaction();
+
+      tbk.initTransaction(1000, 1, 1, 'http://aTestUrl.com', 'http://aFinalTestUrl.com')
+        .then((response) => {
+          expect(response)
+            .toEqual(expect.objectContaining({
+              token: expect.any(String),
+            }));
+          done();
+        })
+        .catch((tbkError) => expect(tbkError).toBeUndefined());
+    });
+  });
+
+  describe('Transacción Mall', () => {
+    it('Ambiente integración', (done) => {
+      const tbk = new Webpay()
+        .withConfiguration(Webpay.Configuration.forTestingWebpayPlusMall())
+        .getMallNormalTransaction();
+
+      tbk.initTransaction(1, 1, 'http://aTestUrl.com', 'http://aFinalTestUrl.com',
+        [
+          {
+            amount: 1000,
+            storeCode: 597044444402,
+            buyOrder: 1,
+          },
+        ])
+        .then((response) => {
+          expect(response)
+            .toEqual(expect.objectContaining({
+              token: expect.any(String),
+            }));
+          done();
+        })
+        .catch((tbkError) => expect(tbkError).toBeUndefined());
+    });
+  });
+});
