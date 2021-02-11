4c8febf03489d4992e9e4b9fdcf9705388660767
Felipe Fiebig <felipe.fiebig@continuum.cl>
2021-02-11 15:32:16 -0300

    Add Oneclick tsdocs

--- lib/transbank/webpay/oneclick/mall_transaction.ts
+++ lib/transbank/webpay/oneclick/mall_transaction.ts
@@ -5,6 +5,15 @@ import TransactionDetail from '../common/transaction_detail';
 import { AuthorizeRequest, StatusRequest, RefundRequest } from './requests';
 
 const MallTransaction = {
+  /**
+   * Authorizes a payment to be charde onto the cardholder's card
+   * @param userName Cardholder's username
+   * @param tbkUser Cardholder's card TBK user assigned by Transbank and returned in
+   * Inscription.finish
+   * @param buyOrder Commerce buy order, make sure this is unique.
+   * @param details Child transactions details, see {@link TransactionDetail} for more information.
+   * @param options (Optional) You can pass options to use a custom configuration for this request.
+   */
   authorize: async (
     userName: string,
     tbkUser: string,
@@ -15,10 +24,25 @@ const MallTransaction = {
     let authorizeRequest = new AuthorizeRequest(userName, tbkUser, buyOrder, details);
     return RequestService.perform(authorizeRequest, options);
   },
+  /**
+   * Obtain the status of a specific transaction
+   * @param buyOrder Child transaction buy order
+   * @param options (Optional) You can pass options to use a custom configuration for this request.
+   */
   status: async (buyOrder: string, options: Options = Oneclick.getDefaultOptions()) => {
     let statusRequest = new StatusRequest(buyOrder);
     return RequestService.perform(statusRequest, options);
   },
+  /**
+   * Request a refund of a specific transaction, if you refund for the full amount and you're within
+   * the time window the transaction will be reversed. If you're past that window or refund for less
+   * than the total amount the transaction will be void.
+   * @param buyOrder Child transaction buy order
+   * @param commerceCode Child commerce code, used to indetify the correct child transaction
+   * @param childBuyOrder Child buy order, used to identify the correct child transaction.
+   * @param amount Amount to be refunded
+   * @param options (Optional) You can pass options to use a custom configuration for this request.
+   */
   refund: async (
     buyOrder: string,
     commerceCode: string,
