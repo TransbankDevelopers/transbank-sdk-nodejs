43ac984b66df6903b10e7aaee546c2672501dbb7
Miguel Vargas <mvarlic@gmail.com>
2024-08-22 20:03:21 -0500

    test: add full transaction tests

---
+++ tests/webpay/transaccion_completa/mall_transaction.test.ts
@@ -0,0 +1,244 @@
+import nock from 'nock';
+import { randomInt } from 'crypto';
+import { CommitDetail, Environment, InstallmentDetail, IntegrationApiKeys, IntegrationCommerceCodes, Options, TransaccionCompleta, TransactionDetail } from '../../../lib';
+import ApiConstants from '../../../lib/transbank/common/api_constants';
+
+describe('MallFullTransactionTest', () => {
+    const apiUrl = `${Environment.Integration}${ApiConstants.WEBPAY_ENDPOINT}`;
+    const option = new Options(IntegrationCommerceCodes.TRANSACCION_COMPLETA_MALL, IntegrationApiKeys.WEBPAY, Environment.Integration);
+    const testToken = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+
+    afterAll(() => {
+        nock.cleanAll();
+    });
+
+    test('create', async () => {
+        const token = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+        const buyOrder = "O-" + randomInt(0, 1000).toString();
+        const sessionId = "S-" + randomInt(0, 1000).toString();
+        const amount = 1000;
+        const cvv = 123;
+        const cardNumber = 'XXXXXXXXXXXX6623';
+        const month = '12';
+        const year = '28';
+
+        const buyOrderMallOne = randomInt(0, 1000).toString();
+        const amountMallOne = 1000;
+        const mallOneCommerceCode = '597055555536';
+
+        const buyOrderMallTwo = randomInt(0, 1000).toString();
+        const amountMallTwo = 1000;
+        const mallTwoCommerceCode = '597055555537';
+
+        let mallDetails = [
+            new TransactionDetail(
+              amountMallOne,
+              mallOneCommerceCode,
+              buyOrderMallOne
+            ),
+            new TransactionDetail(
+              amountMallTwo,
+              mallTwoCommerceCode,
+              buyOrderMallTwo
+            ),
+          ];
+    
+        nock(apiUrl)
+            .post('/transactions')
+            .reply(200, { token });
+    
+        const response = await new TransaccionCompleta.MallTransaction(option).create(
+            buyOrder,
+            sessionId,
+            cardNumber,
+            year + "/" + month,
+            mallDetails,
+            cvv
+          );
+          
+        expect(response.token).toBe(testToken);
+    });
+
+    test('commit', async () => {
+        const buyOrderMallOne = randomInt(0, 1000).toString();
+        const mallOneCommerceCode = '597055555536';
+        const idQueryInstallmentsOne = 1;
+        const deferredPeriodIndexOne = 0;
+
+        const buyOrderMallTwo = randomInt(0, 1000).toString();
+        const mallTwoCommerceCode = '597055555537';
+        const idQueryInstallmentsTwo = 1;
+        const deferredPeriodIndexTwo = 0;
+
+        let mallDetails = [
+            new CommitDetail(
+                mallOneCommerceCode,
+                buyOrderMallOne,
+                idQueryInstallmentsOne,
+                deferredPeriodIndexOne,
+                false
+            ),
+            new CommitDetail(
+                buyOrderMallTwo,
+                mallTwoCommerceCode,
+                idQueryInstallmentsTwo,
+                deferredPeriodIndexTwo,
+                false
+            ),
+          ];
+        const expectedResponse = generateJsonResponse();
+
+        nock(apiUrl)
+            .put(`/transactions/${testToken}`)
+            .reply(200, expectedResponse);
+
+        const response = await new TransaccionCompleta.MallTransaction(option)
+            .commit(testToken, mallDetails);
+
+        expect(response.buy_order).toBe(expectedResponse.buy_order);
+        expect(response.session_id).toBe(expectedResponse.session_id);
+        expect(response.card_detail.card_number).toBe(expectedResponse.card_detail.card_number);
+        expect(response.accounting_date).toBe(expectedResponse.accounting_date);
+        expect(response.accounting_date).toBe(expectedResponse.accounting_date);
+
+        let expectedDetail = expectedResponse.details[0];
+        let detail = response.details[0];
+        expect(detail.amount).toBe(expectedDetail.amount);
+        expect(detail.status).toBe(expectedDetail.status);
+        expect(detail.authorization_code).toBe(expectedDetail.authorization_code);
+        expect(detail.payment_type_code).toBe(expectedDetail.payment_type_code);
+        expect(detail.response_code).toBe(expectedDetail.response_code);
+        expect(detail.installments_number).toBe(expectedDetail.installments_number);
+        expect(detail.commerce_code).toBe(expectedDetail.commerce_code);
+        expect(detail.buy_order).toBe(expectedDetail.buy_order);
+
+        expectedDetail = expectedResponse.details[1];
+        detail = response.details[1];
+        expect(detail.amount).toBe(expectedDetail.amount);
+        expect(detail.status).toBe(expectedDetail.status);
+        expect(detail.authorization_code).toBe(expectedDetail.authorization_code);
+        expect(detail.payment_type_code).toBe(expectedDetail.payment_type_code);
+        expect(detail.response_code).toBe(expectedDetail.response_code);
+        expect(detail.installments_number).toBe(expectedDetail.installments_number);
+        expect(detail.commerce_code).toBe(expectedDetail.commerce_code);
+        expect(detail.buy_order).toBe(expectedDetail.buy_order);
+    });
+
+    test('status', async () => {
+        const expectedResponse = generateJsonResponse();
+
+        nock(apiUrl)
+            .get(`/transactions/${testToken}`)
+            .reply(200, expectedResponse);
+
+        const response = await new TransaccionCompleta.MallTransaction(option)
+            .status(testToken);
+
+        expect(response.vci).toBe(expectedResponse.vci);
+        expect(response.buy_order).toBe(expectedResponse.buy_order);
+        expect(response.session_id).toBe(expectedResponse.session_id);
+        expect(response.card_detail.card_number).toBe(expectedResponse.card_detail.card_number);
+        expect(response.accounting_date).toBe(expectedResponse.accounting_date);
+        expect(response.accounting_date).toBe(expectedResponse.accounting_date);
+
+        let expectedDetail = expectedResponse.details[0];
+        let detail = response.details[0];
+        expect(detail.amount).toBe(expectedDetail.amount);
+        expect(detail.status).toBe(expectedDetail.status);
+        expect(detail.authorization_code).toBe(expectedDetail.authorization_code);
+        expect(detail.payment_type_code).toBe(expectedDetail.payment_type_code);
+        expect(detail.response_code).toBe(expectedDetail.response_code);
+        expect(detail.installments_number).toBe(expectedDetail.installments_number);
+        expect(detail.commerce_code).toBe(expectedDetail.commerce_code);
+        expect(detail.buy_order).toBe(expectedDetail.buy_order);
+
+        expectedDetail = expectedResponse.details[1];
+        detail = response.details[1];
+        expect(detail.amount).toBe(expectedDetail.amount);
+        expect(detail.status).toBe(expectedDetail.status);
+        expect(detail.authorization_code).toBe(expectedDetail.authorization_code);
+        expect(detail.payment_type_code).toBe(expectedDetail.payment_type_code);
+        expect(detail.response_code).toBe(expectedDetail.response_code);
+        expect(detail.installments_number).toBe(expectedDetail.installments_number);
+        expect(detail.commerce_code).toBe(expectedDetail.commerce_code);
+        expect(detail.buy_order).toBe(expectedDetail.buy_order);
+    });
+
+    test('refund', async () => {
+        const url = `/${apiUrl}/transactions/${testToken}/refunds`;
+        const type = 'REVERSED';
+
+        nock(apiUrl)
+            .post(`/transactions/${testToken}/refunds`)
+            .reply(200, { type });
+
+        const childBuyOrder = '500894028';
+        const childCommerceCode = '597055555536';
+        const amount = 1000;
+
+        const response = await new TransaccionCompleta.MallTransaction(option)
+            .refund(testToken, childBuyOrder, childCommerceCode, amount);
+
+        expect(response.type).toBe(type);
+    });
+
+    test('capture', async () => {
+        const expectedResponse = {
+            authorization_code: "1213",
+            authorization_date: "2021-07-31T23:31:14.249Z",
+            captured_amount: 1000,
+            response_code: 0
+        };
+
+        nock(apiUrl)
+            .put(`/transactions/${testToken}/capture`)
+            .reply(200, expectedResponse);
+    
+        const commerceCode = "597055555537";
+        const buyOrder = Math.floor(Math.random() * 1000).toString();
+        const authorization = '1213';
+        const amount = 1000;
+    
+        const response = await new TransaccionCompleta.MallTransaction(option)
+            .capture(testToken, commerceCode, buyOrder, authorization, amount);
+    
+        expect(response.authorization_code).toBe(expectedResponse.authorization_code);
+        expect(response.authorization_date).toBe(expectedResponse.authorization_date);
+        expect(response.captured_amount).toBe(expectedResponse.captured_amount);
+        expect(response.response_code).toBe(expectedResponse.response_code);
+    });
+
+    function generateJsonResponse(): any {
+        return {
+            details: [
+             {
+                amount: 1922,
+                status: "AUTHORIZED",
+                authorization_code: "1213",
+                payment_type_code: "VN",
+                response_code: 0,
+                installments_number: 0,
+                commerce_code: "597055555574",
+                buy_order: "O-36681"
+             },
+             {
+                amount: 1922,
+                status: "AUTHORIZED",
+                authorization_code: "1213",
+                payment_type_code: "VN",
+                response_code: 0,
+                installments_number: 0,
+                commerce_code: "597055555575",
+                buy_order: "O-36682"
+               }
+            ],
+            buy_order: "O-99701",
+            session_id: "S-23531",
+            card_detail: {
+             card_number: "6623"
+            },
+            accounting_date: "0822",
+            transaction_date: "2024-08-23T00:15:56.920Z"
+        };
+    }
+});
---
+++ tests/webpay/transaccion_completa/transaction.test.ts
@@ -0,0 +1,157 @@
+import nock from 'nock';
+import { Environment, IntegrationApiKeys, IntegrationCommerceCodes, Options, TransaccionCompleta } from '../../../lib';
+import { randomInt } from 'crypto';
+import ApiConstants from '../../../lib/transbank/common/api_constants';
+
+describe('FullTransactionTest', () => {
+    const apiUrl = `${Environment.Integration}${ApiConstants.WEBPAY_ENDPOINT}`;
+    const testToken = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    const option = new Options(IntegrationCommerceCodes.TRANSACCION_COMPLETA, IntegrationApiKeys.WEBPAY, Environment.Integration);
+    
+    afterAll(() => {
+        nock.cleanAll();
+    });
+    
+    test('create', async () => {
+        const token = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+        const buyOrder = "O-" + randomInt(0, 1000).toString();
+        const sessionId = "S-" + randomInt(0, 1000).toString();
+        const amount = 1000;
+        const cvv = 123;
+        const cardNumber = 'XXXXXXXXXXXX6623';
+        const month = '12';
+        const year = '28';
+    
+        nock(apiUrl)
+            .post('/transactions')
+            .reply(200, { token });
+    
+        const response = await new TransaccionCompleta.Transaction(option).create(
+            buyOrder,
+            sessionId,
+            amount,
+            cvv,
+            cardNumber,
+            year + "/" + month
+          );
+          
+        expect(response.token).toBe(testToken);
+    });
+    
+    
+    test('commit', async () => {
+        const expectedResponse = generateJsonResponse();  
+        nock(apiUrl)
+            .put(`/transactions/${testToken}`)
+            .reply(200, expectedResponse);
+    
+        const response = await new TransaccionCompleta.Transaction(option).commit(testToken);
+        expect(response.vci).toBe(expectedResponse.vci);
+        expect(response.amount).toBe(expectedResponse.amount);
+        expect(response.status).toBe(expectedResponse.status);
+        expect(response.buy_order).toBe(expectedResponse.buy_order);
+        expect(response.session_id).toBe(expectedResponse.session_id);
+        expect(response.card_detail.card_number).toBe(expectedResponse.card_detail.card_number);
+        expect(response.accounting_date).toBe(expectedResponse.accounting_date);
+        expect(response.transaction_date).toBe(expectedResponse.transaction_date);
+        expect(response.authorization_code).toBe(expectedResponse.authorization_code);
+        expect(response.payment_type_code).toBe(expectedResponse.payment_type_code);
+        expect(response.response_code).toBe(expectedResponse.response_code);
+        expect(response.installments_number).toBe(expectedResponse.installments_number);
+    });
+    
+    test('refund', async () => {
+        const amount = 1000;
+        const type = "REVERSED";
+        const expectedResponse = { type };
+    
+        nock(apiUrl)
+            .post(`/transactions/${testToken}/refunds`)
+            .reply(200, expectedResponse);
+    
+        const response = await new TransaccionCompleta.Transaction(option).refund(testToken, amount);
+        expect(response.type).toBe(type);
+    });
+    
+    test('status', async () => {
+        const expectedResponse = generateJsonResponse(); 
+        nock(apiUrl)
+            .get(`/transactions/${testToken}`)
+            .reply(200, expectedResponse);
+    
+        const response = await new TransaccionCompleta.Transaction(option).status(testToken);
+        expect(response.vci).toBe(expectedResponse.vci);
+        expect(response.amount).toBe(expectedResponse.amount);
+        expect(response.status).toBe(expectedResponse.status);
+        expect(response.buy_order).toBe(expectedResponse.buy_order);
+        expect(response.session_id).toBe(expectedResponse.session_id);
+        expect(response.card_detail.card_number).toBe(expectedResponse.card_detail.card_number);
+        expect(response.accounting_date).toBe(expectedResponse.accounting_date);
+        expect(response.transaction_date).toBe(expectedResponse.transaction_date);
+        expect(response.authorization_code).toBe(expectedResponse.authorization_code);
+        expect(response.payment_type_code).toBe(expectedResponse.payment_type_code);
+        expect(response.response_code).toBe(expectedResponse.response_code);
+        expect(response.installments_number).toBe(expectedResponse.installments_number);
+    });
+
+    test('capture', async () => {
+        const expectedResponse = {
+            authorization_code: "1213",
+            authorization_date: "2021-07-31T23:31:14.249Z",
+            captured_amount: 1000,
+            response_code: 0
+        };
+
+        nock(apiUrl)
+            .put(`/transactions/${testToken}/capture`)
+            .reply(200, expectedResponse);
+    
+
+        const buyOrder = Math.floor(Math.random() * 1000).toString();
+        const authorization = '1213';
+        const amount = 1000;
+    
+        const response = await new TransaccionCompleta.Transaction(option).capture(testToken, buyOrder, authorization, amount);
+    
+        expect(response.authorization_code).toBe(expectedResponse.authorization_code);
+        expect(response.authorization_date).toBe(expectedResponse.authorization_date);
+        expect(response.captured_amount).toBe(expectedResponse.captured_amount);
+        expect(response.response_code).toBe(expectedResponse.response_code);
+    });
+
+    test('installments', async () => {
+        const installmentsNumber = 2;
+        const expectedResponse = { 
+            installments_amount: 615,
+            id_query_installments: 55114821
+         };
+    
+        nock(apiUrl)
+            .post(`/transactions/${testToken}/installments`)
+            .reply(200, expectedResponse);
+    
+        const response = await new TransaccionCompleta.Transaction(option).installments(testToken, installmentsNumber);
+        expect(response.installments_amount).toBe(expectedResponse.installments_amount);
+        expect(response.id_query_installments).toBe(expectedResponse.id_query_installments);
+    });
+    
+    function generateJsonResponse(): any {
+        return {
+            vci: "TSY",
+            amount: 1000.0,
+            status: "AUTHORIZED",
+            buy_order: "1643997337",
+            session_id: "1134425622",
+            card_detail: { card_number: "6623" },  // Asegúrate de que mapResponseDetail esté definido previamente
+            accounting_date: "0731",
+            transaction_date: "2021-07-31T23:31:14.249Z",
+            authorization_code: "1213",
+            payment_type_code: "VD",
+            response_code: 0,
+            installments_number: 0,
+            installments_amount: 1000
+        };
+    }
+});
+
+
