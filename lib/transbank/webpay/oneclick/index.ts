9e644fe6a71315074bef243249dc99aa539c8ba8
Matias Vasquez <m.vasquezac@gmail.com>
2025-05-26 15:44:00 -0400

    feat: update exports

--- lib/transbank/webpay/oneclick/index.ts
+++ lib/transbank/webpay/oneclick/index.ts
@@ -1,8 +1,8 @@
 import _MallInscription from './mall_inscription';
 import _MallTransaction from './mall_transaction';
+import _MallBinInfo from './mall_bin_info';
 
 module Oneclick {
-
   /**
    * Contains methods used to start, finish and delete Inscriptions.
    */
@@ -11,7 +11,10 @@ module Oneclick {
    * Contains methods used to authorize, commit, refund and capture mall Transactions.
    */
   export const MallTransaction: typeof _MallTransaction = _MallTransaction;
-
+  /**
+   * Contains method used to query bin info.
+   */
+  export const MallBinInfo: typeof _MallBinInfo = _MallBinInfo;
 }
 
 export default Oneclick;
